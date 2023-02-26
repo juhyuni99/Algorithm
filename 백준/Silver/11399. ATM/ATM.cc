8f8e8316bcab51f77399c1c028daab836af2e86c
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-02-26 18:30:16 +0900

    Update ATM.cc

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -6,19 +6,18 @@ int n; // 사람의 수
 vector<int> arr; // 비어있는 vector 생성
 
 int main(void) {
-	cin >> n;
-	int result = 0;
-	int sum = 0;
+	cin >> n; 
+	int result = 0; // 각 사람이 돈을 인출하는데 필요한, 시간의 합의 최솟값
+	int sum = 0; //사람마다 걸리는 시간(대기+인출)
 	for (int i = 0; i < n; i++) {
 		int p;
-		
 		cin >> p; // 각 사람이 돈을 인출하는데 걸리는 시간
-		arr.push_back(p); // p값 생성
+		arr.push_back(p); // 각 요소 값 생성하고 p로 초기화
 	}
-	sort(arr.begin(), arr.end());
-	for (int i = 0; i < n; i++) {
-		sum += arr[i];
-		result += sum;// 필요한 시간의 합
+	sort(arr.begin(), arr.end());//오름차순 정렬
+	for (int i = 0; i < n; i++) { 
+		sum += arr[i]; 
+		result += sum;
 	}
-	cout << result << endl;
+	cout << result << endl; //각 사람이 돈을 인출하는데 필요한, 시간의 합의 최솟값 출력
 }
