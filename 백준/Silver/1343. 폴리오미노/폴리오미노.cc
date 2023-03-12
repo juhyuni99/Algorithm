4f642466b6d991db9de0d8fc821cf8f7fb8998c5
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-03-12 15:49:02 +0900

    Update 폴리오미노.cc

--- 백준/Silver/1343. 폴리오미노/폴리오미노.cc
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.cc
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string s;
+string s; //보드판
 
 int main(void) {
 	cin >> s;
 
-	s = regex_replace(s, regex("XXXX"), "AAAA");
-	s = regex_replace(s, regex("XX"), "BB");
-	if (s.find("X")!= std::string::npos) {
+	s = regex_replace(s, regex("XXXX"), "AAAA"); //특정문자 변환 XXXX->AAAA
+	s = regex_replace(s, regex("XX"), "BB"); //특정문자 변환 XX->BB
+	if (s.find("X")!= std::string::npos) { // 변환 후 X가 있으면 -1출력
 		cout << "-1";
 	}
-	else {
+	else { //X가 없으면 그대로 출력
 		cout << s;
 	}
 	return 0;
