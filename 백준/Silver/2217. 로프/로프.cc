beeb7c0a3f31b91925d2e7d518a9e7276d1ea948
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-03-12 18:12:31 +0900

    [Silver IV] Title: 로프, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n; // 로프 개수
+int W_max; // 최대 중량
+
+int main() {
+
+	cin >> n;
+	vector <int> rope_W(n);
+	for (int i = 0; i < n; i++) {
+		cin >> rope_W[i];
+	}
+	sort(rope_W.begin(),rope_W.end()); // 로프의 중량 정렬
+	for (int i = 0; i < rope_W.size(); i++) {
+		W_max = max(W_max, rope_W[n - i - 1] * (i + 1));
+	}
+	cout << W_max;
+}
