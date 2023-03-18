58d6d12c70524ebe9342066d7ca814b089001722
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-03-19 00:27:40 +0900

    Update 에너지 드링크.cc

--- 백준/Silver/20115. 에너지 드링크/에너지 드링크.cc
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.cc
@@ -14,7 +14,7 @@ int main(void) {
 		cin >> X[i];
 	}
 	sort(X.begin(), X.end()); //오름차순
-	for (long unsigned int i = 0; i < X.size(); i++) {
+	for (long unsigned int i = 0; i < X.size(); i++) { // 제일 큰 양의 음료수 병에 다른 음료수를 넣는다.
 		if (i == X.size() - 1) result += X[i];	
 		else result += X[i] / 2;
 	}
