7fa9bb060931370e0c7f40c2e8f0c28cb00a64d5
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-04-03 23:33:05 +0900

    Update 소수 찾기.cc

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -4,21 +4,23 @@ using namespace std;
 
 int main() {
     int N; // 수의 개수
-    int cnt = 0;
+    int cnt = 0; //소수의 개수
     cin >> N;
     vector<int> number(N);
 
     for (int i = 0; i < N; i++) {
-        cin >> number[i];
+        cin >> number[i]; 
         if (number[i] == 1) continue;
+        
+        //소수 판별
         bool is_prime = true;
-        for (int j = 2; j * j <= number[i]; j++) {
-            if (number[i] % j == 0) {
+        for (int j = 2; j * j <= number[i]; j++) { //2부터 N의 제곱근까지의 수 중에서 N이 나누어지는 수가 있는지 검사
+            if (number[i] % j == 0) { //나누어지면 소수가 아니다.
                 is_prime = false;
                 break;
             }
         }
-        if (is_prime) {
+        if (is_prime) { 
             cnt++;
         }
     }
