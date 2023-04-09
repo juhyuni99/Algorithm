37c2d80c8834e1e826ad496edc3dcc02ce807f0e
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-04-09 17:53:32 +0900

    Update 행복 유치원.cc

--- 백준/Gold/13164. 행복 유치원/행복 유치원.cc
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.cc
@@ -4,21 +4,21 @@
 using namespace std;
 
 int main(void) {
-	int N, K;
+	int N, K; // 학생수, 조의 수
 	cin >> N >> K;
-	vector<int> n(N);
-	vector<int> cost(N-1);
+	vector<int> n(N); // 원생들의 키
+	vector<int> cost(N-1); // 옆사람과의 키 차이
 	for (int i = 0; i < N; i++) {
 		cin >> n[i];
 		if (i >= 1) {
-			cost[i - 1] = n[i] - n[i - 1]; // 키 차이 저장
+			cost[i - 1] = n[i] - n[i - 1]; // 옆사람과의 키 차이 저장
 		}
 	}
-	sort(cost.begin(), cost.end());
+	sort(cost.begin(), cost.end()); // 오름차순 정렬
 	long long result = 0;
-	for (int i = 0; i < N - K; i++) {
+	for (int i = 0; i < N - K; i++) { //N-K개수를 제외한 나머지 키 차이의 합
 		result += cost[i];
 	}
-	cout << result;
+	cout << result; //최소비용
 	return 0;
 }
