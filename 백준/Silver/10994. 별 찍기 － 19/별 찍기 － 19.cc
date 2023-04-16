5361bd48faf6400735f8d406912632663caa39e6
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-04-16 20:40:14 +0900

    [Silver IV] Title: 별 찍기 - 19, Time: 4 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Silver/10994. 별 찍기 － 19/별 찍기 － 19.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+
+map<int, int> N;
+
+char arr[397][397]; // n의 최댓값 4n-3 = 4*100-3 = 397
+void draw(int len, int row, int col) {
+	for (int i = 0; i < len; i++) {
+		if (i == 0 || i == len - 1) { // 윗 변, 아랫 변
+			for (int j = 0; j < len; j++) {
+				arr[row + i][col + j] = '*';
+			}
+		}
+		else { //높이 : 별 두개 찍기
+			arr[row + i][col] = '*'; //첫번째 열
+			arr[row + i][col + len - 1] = '*'; // 마지막 열
+		}
+	}
+}
+
+void square(int N, int row, int col) {
+	int len = 4 * N - 3;
+	draw(len, row, col); // 재귀 전에 현재 지점의 사각형 먼저 그리기
+	if (N == 1) { // n이 1이면 별 1개
+		return;
+	}
+	square(N - 1, row + 2, col + 2); // 다음 사각형으로 재귀시 다음 재귀 시작 지점 인수로 넘겨주기
+}
+
+int main(void) {
+	int n; //숫자
+	cin >> n;
+
+	//별 찍기 전에 미리 전체 배열에 공백 넣어주기
+	for (int i = 0; i < 4*n-3; i++) {
+		for (int j = 0; j < 4 * n - 3; j++) {
+			arr[i][j] = ' ';
+		}
+
+	}
+	square(n, 0, 0); // 별 찍기. 가장 큰 사각형부터 넘기기. 시작점은 좌상단.
+
+	//출력
+	for (int i = 0; i < 4 * n - 3; i++) {
+		for (int j = 0; j < 4 * n - 3; j++) {
+			cout << arr[i][j];
+		}
+		cout << '\n';
+	}
+	return 0;
+}
