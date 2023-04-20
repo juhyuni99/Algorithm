0e94b7df4cefb2073e63cc70b0913d125f9f721d
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-04-20 16:10:02 +0900

    Update 스위치 켜고 끄기.cc

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.cc
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.cc
@@ -2,52 +2,56 @@
 #include <algorithm>
 
 using namespace std;
+int swch[101]; //스위치 상태
+int chil; // 학생 수
+int n; //스위치 개수
 
-int arr[101];
-int N;
+//남학생
+void boy(int num) {
+	//받은 수의 배수 부분의 스위치 상태를 바꿔줌
+	for (int i = 1; i * num <= n; i++) {
+		swch[num*i] = !swch[num*i];
+	}
+}
 
-void ifGirl(int n) { //여자
-	int left = n - 1;
-	int right = n + 1;
-	arr[n] = !arr[n];
-	while (left >= 1 && right <= N) {
-		if (arr[left] != arr[right]) {
-			break;
+//여학생
+void girl(int num) {
+	swch[num] = !swch[num]; // 받은 수 먼저 상태를 바꿔 줌.
+	//범위 주의
+	for (int i = 1; num - i >= 1 && num + i <= n; i++) {
+		//받은 수를 기준으로 양옆의 스위치 상태가 같으면 상태를 바꿔줌
+		if (swch[num - i] == swch[num + i]) {
+			swch[num - i] = !swch[num - i];
+			swch[num + i] = !swch[num + i];
 		}
-		arr[left] = !arr[left];
-		arr[right] = !arr[right];
-		left--;
-		right++;
+		//스위치 상태가 다르면 for문 탈출
+		else break;
 	}
 }
 
-void ifBoy(int n) { //남자
-	for (int i = n; i <= N; i += n) {
-		arr[i] = !arr[i];
-	}
-}
-
-int main() {
+int main(void) {
 	
-	cin >> N;
-	for (int i = 1; i <= N; i++) {
-		cin >> arr[i];
+	cin >> n; 
+	for (int i = 1; i <= n; i++) { // 스위치 번호 시작 1번부터
+		cin >> swch[i]; // 스위치 상태
 	}
-	int type;
-	cin >> type;
-	for (int i = 0; i < type; i++) {
-		int sex, n;
-		cin >> sex >> n;
-		if (sex == 1) {
-
-			ifBoy(n);
+	cin >> chil;  
+	for (int i = 0; i < chil; i++) {
+		int gender, num; // 학생 성별, 학생이 받은 수 
+		cin >> gender >> num;
+		if (gender == 1) { // 남학생
+			boy(num); //함수 호출
 		}
-		else ifGirl(n);
+		else if (gender == 2) { //여학생
+			girl(num); //함수 호출
+		}
 	}
 
-	for (int i = 1; i <= N; i++) {
-		cout << arr[i] << " ";
-		if (i % 20 == 0) cout << "\n";
+	//스위치 상태 출력
+	for (int i = 1; i <= n; i++) {
+		cout << swch[i] << " ";
+		if (i % 20 == 0) { // 한줄에 20개씩 출력, 개행
+			cout << '\n';
+		}
 	}
-	return 0;
 }
