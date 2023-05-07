27c0105237ed9e08bc7d041e943c323df81545fd
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-05-07 20:26:01 +0900

    [Silver III] Title: 달팽이, Time: 140 ms, Memory: 5936 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.cc
+++ 백준/Silver/1913. 달팽이/달팽이.cc
@@ -1,25 +1,59 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int a[1001][1001], n, m, dir, rety, retx, y, x;
-const int dy[] = {1, 0, -1, 0};
-const int dx[] = {0, 1, 0, -1};
+int n, x, y, dir, findNum;
+int cnt = 1, siz = 1;
+int map[1001][1001];
+
+//그려진 표 출력
+void print() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << map[i][j] << ' ';
+        cout << '\n';
+    }
+}
+
+//달팽이 그리기
+void make() {
+    x = n / 2;
+    y = n / 2;
+    map[x][y] = 1;
+
+    while (cnt < n * n) {
+        //왼쪽 변
+        if (dir == 0) {
+            for (int k = 0; k < siz; k++) {
+                map[--x][y] = ++cnt;
+            }
+        }
+        //윗변
+        else if (dir == 1) {
+            for (int k = 0; k < siz; k++) map[x][++y] = ++cnt;
+            siz++;
+        }
+        //오른쪽 변
+        else if (dir == 2) {
+            for (int k = 0; k < siz; k++) map[++x][y] = ++cnt;
+        }
+        //아랫변
+        else if (dir == 3) {
+            for (int k = 0; k < siz; k++)
+                map[x][--y] = ++cnt;
+            siz++;
+        }
+        dir = (dir + 1) % 4; //방향 전환
+    }
+}
 int main() {
-    cin >> n >> m;
-    for(int i = n * n; i > 0; i--){
-        a[y][x] = i;
-        if(i == m){rety = y + 1; retx = x + 1;}
-        if(y + dy[dir] < 0 || y + dy[dir] >= n || x + dx[dir] < 0 || x + dx[dir] >= n || a[y + dy[dir]][x + dx[dir]] != 0){
-            dir = (dir + 1) % 4;
+    cin >> n >> findNum;
+    make();
+    print();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (map[i][j] == findNum) {
+                cout << i + 1 << ' ' << j + 1; //위치 출력
+                break;
+            }
         }
-        y = y + dy[dir];
-        x = x + dx[dir];
     }
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cout << a[i][j] << " ";
-        }
-        cout <<"\n";
-    }
-    cout << rety << " " << retx << "\n";
-    return 0;
 }
