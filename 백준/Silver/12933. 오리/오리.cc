369fca94609b38e421ffc69b99d21ccb90a721a4
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-05-09 23:17:07 +0900

    Update 오리.cc

--- 백준/Silver/12933. 오리/오리.cc
+++ 백준/Silver/12933. 오리/오리.cc
@@ -1,36 +1,42 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    string s; cin >> s;
+    string s; //녹음된 울음소리
+    cin >> s;
+
     const string DB = "quack";
     int n = s.size();
     vector<int> used(n), cnt(n);
     bool result = true;
-    for(int i=0;i<n;i++){
-        if(s[i] != 'q')continue;
+
+    for (int i = 0; i < n; i++) {
+        if (s[i] != 'q')continue;
         int R = i, idx = 0;
-        while(idx < 5 && R < n){
-            if(used[R] == 0 && s[R] == DB[idx]){
+        while (idx < 5 && R < n) {
+            if (used[R] == 0 && s[R] == DB[idx]) {
                 used[R] = 1;
                 idx++;
             }
             R++;
         }
-        if(idx != 5) result = false;
-        for(int j=i;j<R;j++) cnt[j]++;
+        if (idx != 5) result = false;
+        for (int j = i; j < R; j++) cnt[j]++;
     }
     int ans = 0;
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++) {
         ans = max(ans, cnt[i]);
-        if(cnt[i] == 0)result = false;
+        if (cnt[i] == 0)result = false;
     }
-    
-    if(result)cout << ans;
+
+    if (result) cout << ans;
     else cout << -1;
     return 0;
 }
