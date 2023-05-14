ce100e9ccb219f4f4a662155739787c3d5d3df5f
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-05-14 17:29:04 +0900

    Update 원상 복구 （small）.cc

--- 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.cc
+++ 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.cc
@@ -1,26 +1,27 @@
 #include <iostream>
 #include <memory.h>
 using namespace std;
+// i번째로 가지고 오기, K번 카드를 섞은 후 카드 배치, 원래 카드의 배치
 int D[10001], S[100001], P[1000001];
 
 int main() {
 	int n, k; // 카드 개수, 섞은 횟수
 	cin >> n >> k;
-	for (int i = 1; i <= n; i++) {
-		cin >> S[i];
+	for (int i = 1; i <= n; i++) { 
+		cin >> S[i];// K번 카드를 섞은 후 카드 배치
 	}
 	for (int i = 1; i <= n; i++) {
-		cin >> D[i];
+		cin >> D[i];// i번째로 가지고 오기
 	}
-	while (k--) {
+	while (k--) {//카드 섞은 횟수 만큼 반복
 		for (int i = 1; i <= n; i++) {
-			P[D[i]] = S[i];
+			P[D[i]] = S[i]; // 한 번 섞었을 때의 결과
 		}
-		memcpy(S, P, sizeof(S));
+		memcpy(S, P, sizeof(S));//S에 K 메모리 복사
 	}
 	
 	for (int i = 1; i <= n; i++) {
-		cout << P[i] << " ";
+		cout << P[i] << " "; // 원래 카드의 배치 출력
 	}
 	return 0;
 }
