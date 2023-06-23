abbb6573b52a7fe533c0c90419dde471e5810ccc
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-06-23 19:11:45 +0900

    Update 바이러스.cc

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
 
 using namespace std;
-bool visited[101];
-vector<int> graph[101];
-int num;
+bool visited[101]; //방문 check
+vector<int> graph[101]; //컴퓨터 정보
+int num;// 개수
 
+//dfs 함수
 void dfs(int x) {
 	visited[x] = true;
 	for (int i = 0; i < graph[x].size(); i++) {
 		int y = graph[x][i];
-		if (!visited[y]) {
+		if (!visited[y]) { //방문하지 않았을 경우
 			dfs(y);
 			num++;
 		}
@@ -21,13 +22,13 @@ int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	int com, path;
-	cin >> com >> path;
-	for (int i = 0; i < path; i++) {
+	cin >> com >> path; //컴퓨터 수, 컴퓨터 쌍의 수
+	for (int i = 0; i < path; i++) { //컴퓨터 번호 쌍
 		int a,b;
 		cin >> a >> b;
-		graph[a].push_back(b);
+		graph[a].push_back(b); //정보 추가
 		graph[b].push_back(a);
 	}
-	dfs(1);
+	dfs(1); //함수 호출
 	cout << num;
 }
