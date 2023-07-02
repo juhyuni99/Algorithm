435fa681aa1105fc2907b5071e072d25020f89fa
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-07-02 21:57:54 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 108 ms, Memory: 8024 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -1,36 +1,43 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<queue>
+#include<vector>
 
 using namespace std;
-#define MAX 100001
+vector<int> graph[100001]; //그래프
+bool visit[100001];//방문 표시
+int res[100001];// 부모노드 값 저장
 
-int N;
-int arr[MAX];
-bool visited[MAX];
-vector<int> v[MAX];
-
-void dfs(int k) {
-    visited[k]=true;
-    for(int i=0;i<v[k].size();i++) {
-        int next = v[k][i];
-        if(!visited[next]) {
-            arr[next]=k;
-            dfs(next);
-        }
-    }
+//bfs 탐색
+void bfs() {
+	queue<int> q;
+	visit[1] = true;
+	q.push(1);
+	while (!empty(q)) { //큐가 비어있지 않을 때까지 반복
+		int parent = q.front();
+		q.pop();
+		for (int i = 0; i < graph[parent].size(); i++) { //자식노드 검사
+			int child = graph[parent][i];
+			if (!visit[child]) {//방문하지 않았으면
+				res[child] = parent;
+				visit[child] = true;
+				q.push(child);
+			}
+		}
+	}
 }
 
 int main() {
-    cin >> N;
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+	bfs();
+	for (int i = 2; i <= N; i++) {
+		cout << res[i] << "\n";
+	}
 
-    for(int i=0;i<N;i++) {
-        int x,y;
-        cin >> x >> y;
-        v[x].push_back(y);
-        v[y].push_back(x);
-    }
-
-    dfs(1);
-
-    for(int i=2;i<=N;i++) cout << arr[i] << "\n";
 }
