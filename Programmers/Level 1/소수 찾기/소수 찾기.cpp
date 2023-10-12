b7ff4e07cf58a10224b62d04616ebdde56ffe545
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-10-12 14:44:47 +0900

    Update 소수 찾기.cpp

--- Programmers/Level 1/소수 찾기/소수 찾기.cpp
+++ Programmers/Level 1/소수 찾기/소수 찾기.cpp
@@ -1,5 +1,26 @@
 #include <string>
 #include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<bool> temp(n+1, true); // n+1크기만큼 메모리할당, true로 초기화
+    for(int i= 2; i<=n; i++){ //2~n까지 소수판별
+        if(temp[i]==true){ 
+            for(int j=2; j*i<=n; j++){ //중복 제거, n의 제곱근까지 확인
+                temp[j*i]=false; // i의 배수이면 소수가 아님
+            }
+            answer++; // 소수의 개수 세기
+        }
+    }
+    return answer;
+}
+
+
+/*
+#include <string>
+#include <vector>
 #include <cmath>
 
 using namespace std;
@@ -22,3 +43,4 @@ int solution(int n) {
 	
     return answer;
 }
+*/
