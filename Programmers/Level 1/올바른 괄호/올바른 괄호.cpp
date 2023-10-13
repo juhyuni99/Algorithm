9faf8f9b82dd31cdd20a55c81856abf568fae4f6
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-10-13 20:33:07 +0900

    Create 올바른 괄호.cpp

---
+++ Programmers/Level 1/올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,20 @@
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    int cnt = 0;
+    for(int i = 0; i < s.length(); i++) {
+        if (s[i] == '(')
+            cnt++;
+        else if (s[i] == ')')
+            cnt--;
+
+        if (cnt < 0)
+            return false;
+
+    }
+    return cnt == 0;
+}
