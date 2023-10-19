9be6515995f8f90cb69932e5ec86fae87fe8e57e
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-10-19 13:11:55 +0900

    Update 완주한지못한 선수.cpp

--- Programmers/Level 1/완주하지못한 선수/완주한지못한 선수.cpp
+++ Programmers/Level 1/완주하지못한 선수/완주한지못한 선수.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
-    map<string, int> m;
+    map<string, int> m; //map함수 이용
     for(int i=0; i<participant.size(); i++){
-        m[participant[i]]++;
+        m[participant[i]]++; //value값 증가
     }
     for(int i=0; i<completion.size(); i++){
-        m[completion[i]]--;
+        m[completion[i]]--; //value값 감소
     }
-    for(auto iter:m){
-        if(iter.second==1){
+    for(auto iter:m){ // map함수 key확인
+        if(iter.second==1){ //value값이 1이면 
             answer = iter.first;
         }
     }
