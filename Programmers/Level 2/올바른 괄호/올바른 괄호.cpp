9640c492a3cced70654e83e067b4bad194690d87
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-10-25 13:30:38 +0900

    Update 올바른 괄호.cpp

--- Programmers/Level 2/올바른 괄호/올바른 괄호.cpp
+++ Programmers/Level 2/올바른 괄호/올바른 괄호.cpp
@@ -5,16 +5,25 @@ using namespace std;
 
 bool solution(string s)
 {
-    int cnt = 0;
-    for(int i = 0; i < s.length(); i++) {
-        if (s[i] == '(')
+    bool answer = true;
+    int cnt=0;
+    for(int i=0; i<s.length(); i++){
+        if(s[i]=='('){ //카운트 증가
             cnt++;
-        else if (s[i] == ')')
+        }
+        else if(s[i]==')'){ //카운트 감소
             cnt--;
-
-        if (cnt < 0)
-            return false;
-
+        }
+        if(cnt<0){ //')'이 먼저 나오거나 '('보다 더 많이 나온 경우 false
+            answer = false;
+            return answer;
+        }
     }
-    return cnt == 0;
+    
+    if(cnt==0){ // 올바른 괄호일 경우 true
+        answer = true;
+    }
+    else answer = false;
+    return answer;
+    
 }
