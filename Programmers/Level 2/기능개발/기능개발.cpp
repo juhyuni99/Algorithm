830940d41b6b218f688fda763ee60df35dca8d18
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-10-25 15:54:39 +0900

    Update 기능개발.cpp

--- Programmers/Level 2/기능개발/기능개발.cpp
+++ Programmers/Level 2/기능개발/기능개발.cpp
@@ -9,23 +9,22 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
     int res = 0; 
     for(int i=0; i<progresses.size(); i++){
         res = 100 - progresses[i];
-        if(res/speeds[i] ==0){
-            cnt[i]=res/speeds[i];
+        if(res/speeds[i]==0){
+            cnt.push_back(res/speeds[i]);
         }
         else{
-            cnt[i]=res/speeds[i]+1;
+            cnt.push_back(res/speeds[i]+1);
         }
     }
     answer.push_back(1);
     int cnt1=0;
     for(int i=1; i<cnt.size(); i++){
         if(cnt[i-1]<cnt[i]){
-            cnt1=0;
-            answer[i]++;
+            answer.push_back(1);
+            cnt1++;
         }
         else{
-            cnt1++;
-            answer[i-cnt1]++;
+            answer[cnt1]++;
         }
     }
     return answer;
