670d3b46991d3a57beebec1768841966ee2c6492
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-10-26 23:52:47 +0900

    Update k번째 수.cpp

--- Programmers/Level 1/k번째 수/k번째 수.cpp
+++ Programmers/Level 1/k번째 수/k번째 수.cpp
@@ -7,7 +7,7 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     for(int i=0; i<commands.size(); i++){
         vector<int> ans; // 정렬된 수 저장
-        for(int j=commands[i][0]-1; j<commands[i][1]; j++){ // 원하는 길이만큼 반복분을 이용하여 ans에 값 저장
+        for(int j=commands[i][0]-1; j<commands[i][1]; j++){ // 원하는 길이만큼 반복문을 이용하여 ans에 값 저장
             ans.push_back(array[j]);
         }
         sort(ans.begin(), ans.end());// 오름차순 정렬
