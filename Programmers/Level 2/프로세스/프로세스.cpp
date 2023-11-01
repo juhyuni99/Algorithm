ae298b7c824cfcfffb6ca392680655f3adf4891c
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-11-01 18:53:10 +0900

    Update 프로세스.cpp

--- Programmers/Level 2/프로세스/프로세스.cpp
+++ Programmers/Level 2/프로세스/프로세스.cpp
@@ -10,16 +10,16 @@ int solution(vector<int> priorities, int location) {
     for(int i=0; i<priorities.size(); i++){
         q.push(priorities[i]);
     }
-    sort(priorities.begin(), priorities.end(), greater<int>());
-    int answer = 0;
+    sort(priorities.begin(), priorities.end(), greater<int>()); //내림차순 정렬
+    int answer = 0; //location 해당 프로세스가 몇 번째로 실행
     while(!q.empty()){
-        if(priorities[0]!=q.front()){
+        if(priorities[0]!=q.front()){ //우선순위가 높은 수와 queue값이 다를 때
             location--;
             q.push(q.front());
             q.pop();
-            if(location<0) location = priorities.size()-1;
+            if(location<0) location = priorities.size()-1; //2번째 테스트케이스 경우 location이 음수가 되버리면 q에 다시 push가 되어 priorities사이즈-1번째 인덱스로 변한다.
         }
-        else{
+        else{ //우선순위가 높은 수와 queue값이 같을 때
             answer++;
             q.pop();
             priorities.erase(priorities.begin());
